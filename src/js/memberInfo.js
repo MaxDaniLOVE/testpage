b332ea53f41d225f7c363ef75c31f93665100336
MaxDaniLOVE <mahseeen@gmail.com>
2020-03-27 13:37:33 +0300

    fix: fix styles for members pop-up

--- src/js/memberInfo.js
+++ src/js/memberInfo.js
@@ -6,8 +6,10 @@ const displayMemberInfo = (block) => {
     <div class="team__grid__inner-block">
       <div class="team__grid__inner-block_triangle"></div>
       <div class="team__grid__inner-block_main">
-        <h4 class="team__grid__inner-block_title">AL RAYHAN</h4>
-        <p class="team__grid__inner-block_subtitle">/ UI Designer</p>
+        <div class="team__grid__inner-block_title">
+          <h4>AL RAYHAN</h4>
+          <p>/ UI Designer</p>
+        </div>
         <p class="team__grid__inner-block_body">Lorem Ipsum is not simply is  an action designer random text It has roots in a piece.</p>
         <div class="team__grid__inner-block_links-block">
           <a href="#" class="team__grid__inner-block_links-block_link">
